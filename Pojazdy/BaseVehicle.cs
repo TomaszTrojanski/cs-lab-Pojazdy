1a1da86f5cc0dd900d4c81ed8871890de16f1842
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-05-02 11:08:29 +0200

    work - base vehicle class, base class constructor

--- Pojazdy/BaseVehicle.cs
+++ Pojazdy/BaseVehicle.cs
@@ -6,10 +6,46 @@ namespace Pojazdy
 {
     public abstract class BaseVehicle : IVehicle
     {
-        public string Name { get; }
-        public bool IsActive { get; }
+        private string name;
+        private bool isMoving;
+        private double currentVelocity;
+        public string Name { get => name; }
+        public bool IsMoving { get => isMoving; }
+        public double CurrentVelocity { get => currentVelocity; }
         public double MinVelocity { get; }
         public double MaxVelocity { get; }
         public Environment Environment { get; }
+        public Engine Engine { get; }
+        public void StartStop()
+        {
+            if (isMoving)
+            {
+                currentVelocity = 0;
+                Console.WriteLine($"Vehicle {Name} stopped");
+            }
+            else
+            {
+                currentVelocity = MinVelocity;
+                Console.WriteLine($"Vehicle {Name} started\nCurrent velocity: {currentVelocity}");
+            }
+            isMoving = !isMoving;
+        }
+
+        public void ChangeVelocity(double acceleration)
+        {
+            currentVelocity += acceleration;
+            if (currentVelocity > MaxVelocity)
+                currentVelocity = MaxVelocity;
+            if (currentVelocity < MinVelocity)
+                currentVelocity = MinVelocity;
+        }
+        public BaseVehicle(string name, double minVelocity = 1d, double maxVelocity = 40d, Environment environment = Environment.Ground, Engine engine = null)
+        {
+            this.name = name;
+            this.MinVelocity = minVelocity;
+            this.MaxVelocity = maxVelocity;
+            this.Environment = environment;
+            this.Engine = engine;
+        }
     }
 }
